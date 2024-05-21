c99a36ebd944f74e3bc628701232b8a9db45d1a8
JulianFernandezMakusas <jfernandezmokusas@frba.utn.edu.ar>
2024-05-21 17:11:14 -0300

    Barra terminada sin routing, foto recortada aunque hay que revisar con delfi cual prefiere. Falta cargar las otras vistas de los proyectos y la vista de contactame

--- src/app/proyectos/branding/branding.component.ts
+++ src/app/proyectos/branding/branding.component.ts
@@ -10,11 +10,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './branding.component.css'
 })
 export class BrandingComponent {
+  widthNum = 447;
+  heightNum = 447;
   images = [
-    {url: '../../assets/Proyectos/Branding/Agustina.jpg', title: "Agustina", width: "", height: ""},
-    {url: '../../assets/Proyectos/Branding/AntiQ.jpg', title: "AntiQ", width: "", height: ""},
-    {url: '../../assets/Proyectos/Branding/CCRecoleta.jpg', title: "CCRecoleta", width: "", height: ""},
-    {url: '../../assets/Proyectos/Branding/ElAlmacen.jpg', title: "ElAlmacen", width: "", height: ""},
-    {url: '../../assets/Proyectos/Branding/Lilith.jpg', title: "Lilith", width: "", height: ""},
+    {url: '../../assets/Proyectos/Branding/Agustina.jpg', title: "Agustina", width: "1", height: "1"},
+    {url: "../../assets/Proyectos/Branding/Refasi.jpg", title: "Refasi", width: "1", height: "1"},
+    {url: '../../assets/Proyectos/Branding/Nace.jpg', title: "Nace", width: "1", height: "2"},
+    {url: '../../assets/Proyectos/Branding/ElAlmacen.jpg', title: "ElAlmacen", width: "1", height: "1"},
+    {url: '../../assets/Proyectos/Branding/Refazi.jpg', title: "Refazi", width: "1", height: "1"},
+    {url: '../../assets/Proyectos/Branding/Proteger.jpg', title: "Proteger", width: "2", height: "1"},
+    {url: '../../assets/Proyectos/Branding/TechStore.jpg', title: "TechStore", width: "1", height: "1"},
+    {url: '../../assets/Proyectos/Branding/Lilith.jpg', title: "Lilith", width: "1", height: "1"},
+    {url: '../../assets/Proyectos/Branding/CCRecoleta.jpg', title: "CCRecoleta", width: "1", height: "1"},
+    {url: '../../assets/Proyectos/Branding/AntiQ.jpg', title: "AntiQ", width: "1", height: "1"},
   ]
 }
