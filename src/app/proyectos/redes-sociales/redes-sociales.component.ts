894a85bf8e88ff960a3753184daeff92cc91c7c7
JulianFernandezMakusas <jfernandezmokusas@frba.utn.edu.ar>
2024-05-31 23:47:18 -0300

    Pagina terminada de verdad

--- src/app/proyectos/redes-sociales/redes-sociales.component.ts
+++ src/app/proyectos/redes-sociales/redes-sociales.component.ts
@@ -1,33 +1,35 @@
 import { Component } from '@angular/core';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatDialog } from '@angular/material/dialog';
+import { ImageModalComponent } from '../../image-modal/image-modal.component';
 
 
 @Component({
   selector: 'app-redes-sociales',
   standalone: true,
-  imports: [MatGridListModule],
+  imports: [MatGridListModule, ImageModalComponent],
   templateUrl: './redes-sociales.component.html',
   styleUrl: './redes-sociales.component.css'
 })
 export class RedesSocialesComponent {
-  calcTamanio(multiplicador: any) {
-    if(multiplicador > 1) {
-      return multiplicador*447 + 48
-    } else {
-      return multiplicador*447
-    }
-  }
+  constructor(public dialog: MatDialog){}
   
   images = [
-    {url: '../../assets/Proyectos/Branding/Agustina.jpg', title: "Agustina", width: this.calcTamanio(1), height: this.calcTamanio(1)},
-    {url: "../../assets/Proyectos/Branding/Refasi.jpg", title: "Refasi", width: this.calcTamanio(1), height: this.calcTamanio(1)},
-    {url: '../../assets/Proyectos/Branding/Nace.jpg', title: "Nace", width: this.calcTamanio(1), height: this.calcTamanio(2)},
-    {url: '../../assets/Proyectos/Branding/ElAlmacen.jpg', title: "ElAlmacen", width: this.calcTamanio(1), height: this.calcTamanio(1)},
-    {url: '../../assets/Proyectos/Branding/Refazi.jpg', title: "Refazi", width: this.calcTamanio(1), height: this.calcTamanio(1)},
-    {url: '../../assets/Proyectos/Branding/Proteger.jpg', title: "Proteger", width: this.calcTamanio(2), height: this.calcTamanio(1)},
-    {url: '../../assets/Proyectos/Branding/TechStore.jpg', title: "TechStore", width: this.calcTamanio(1), height: this.calcTamanio(1)},
-    {url: '../../assets/Proyectos/Branding/Lilith.jpg', title: "Lilith", width: this.calcTamanio(1), height: this.calcTamanio(1)},
-    {url: '../../assets/Proyectos/Branding/CCRecoleta.jpg', title: "CCRecoleta", width: this.calcTamanio(1), height: this.calcTamanio(1)},
-    {url: '../../assets/Proyectos/Branding/AntiQ.jpg', title: "AntiQ", width: this.calcTamanio(1), height: this.calcTamanio(1)},
+    {url: '../../assets/Proyectos/Branding/Agustina.jpg', title: "Agustina"},
+    {url: "../../assets/Proyectos/Branding/Refasi.jpg", title: "Refasi"},
+    {url: '../../assets/Proyectos/Branding/Nace.jpg', title: "Nace"},
+    {url: '../../assets/Proyectos/Branding/ElAlmacen.jpg', title: "ElAlmacen"},
+    {url: '../../assets/Proyectos/Branding/Refazi.jpg', title: "Refazi"},
+    {url: '../../assets/Proyectos/Branding/Proteger.jpg', title: "Proteger"},
+    {url: '../../assets/Proyectos/Branding/TechStore.jpg', title: "TechStore"},
+    {url: '../../assets/Proyectos/Branding/Lilith.jpg', title: "Lilith"},
+    {url: '../../assets/Proyectos/Branding/CCRecoleta.jpg', title: "CCRecoleta"},
+    {url: '../../assets/Proyectos/Branding/AntiQ.jpg', title: "AntiQ"},
   ]
+
+  abrirImagen(imageSrc: string, titulo: string,description: string): void {
+    this.dialog.open(ImageModalComponent, {
+      data: { imageSrc, titulo, description }
+    });
+  }
 }
