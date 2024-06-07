9d66577db55754b2c364810c4e2b30abd5e31dc1
JulianFernandezMakusas <jfernandezmokusas@frba.utn.edu.ar>
2024-06-06 21:52:41 -0300

    Textos de las imagenes

--- src/app/image-modal/image-modal.component.ts
+++ src/app/image-modal/image-modal.component.ts
@@ -1,19 +1,31 @@
-import { Component, Inject } from '@angular/core';
+import { Component, ElementRef, Inject, ViewChild, viewChild } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-image-modal',
   standalone: true,
+  template: `
+    <h1>{{ data.titulo }}</h1>
+    <img [src]="data.imageSrc" alt="Image">
+    <p id="description"></p>
+  ` ,
   imports: [],
   templateUrl: './image-modal.component.html',
   styleUrl: './image-modal.component.css'
 })
 export class ImageModalComponent {
+  @ViewChild('description', { static: true }) descriptionElement!: ElementRef;
+
   constructor(
     public dialogRef: MatDialogRef<ImageModalComponent>,
-    @Inject(MAT_DIALOG_DATA) public data: { imageSrc: string, titulo:string,description: string }
+    @Inject(MAT_DIALOG_DATA) public data: { imageSrc: string, titulo:string, description: string }
   ) {}
+  ngOnInit() {
+    if (this.descriptionElement && this.descriptionElement.nativeElement) {
+      this.descriptionElement.nativeElement.innerHTML = this.data.description;
+    }
 
+  }
   close(): void {
     this.dialogRef.close();
   }
