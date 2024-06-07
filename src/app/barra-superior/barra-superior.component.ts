b1e8803d890164f800b4179018e7aa8f67f3bc96
JulianFernandezMakusas <jfernandezmokusas@frba.utn.edu.ar>
2024-06-07 15:23:54 -0300

    Barra de navegacion actualizada

--- src/app/barra-superior/barra-superior.component.ts
+++ src/app/barra-superior/barra-superior.component.ts
@@ -1,7 +1,7 @@
-import { Component, NgModule, ViewChild } from '@angular/core';
+import { Component, HostListener, NgModule, ViewChild } from '@angular/core';
 import { MatToolbar } from '@angular/material/toolbar'
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { Router } from '@angular/router';
 import { RouterModule } from '@angular/router';
 import { ViewportScroller } from '@angular/common';
@@ -17,15 +17,15 @@ import { interval } from 'rxjs';
 
 export class BarraSuperiorComponent {
   title = 'ruta';
+  lastScrollTop = 0;
+  isToolbarHidden = true;
+  hideTimeout: any;
 
   constructor(private router: Router, private viewportScroller: ViewportScroller) {}
 
   scrollTo(section: string){
     this.router.navigate([], {fragment: section});
   }
-
-  isToolbarHidden = true;
-  hideTimeout: any;
 
   checkMousePosition(event: MouseEvent) {
     const threshold = 100;
@@ -49,5 +49,16 @@ export class BarraSuperiorComponent {
     }, 500);
   }
 
+  @HostListener('window:scroll', [])
+  onWindowScroll() {
+    const st = window.pageYOffset || document.documentElement.scrollTop;
+
+    if (st > this.lastScrollTop) {
+      this.isToolbarHidden = true;
+    } else {
+      this.isToolbarHidden = false;
+    }
+    this.lastScrollTop = st <= 0 ? 0 : st;
+  }
 
 }
